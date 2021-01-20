2400f2675b84485e99bf979f6ca4620620653f90
Marc Perrien <marc@chaosiq.io>
2021-01-20 19:04:02 +0100

    Header styles. (#3)
    Signed-off-by: Marc Perrien <marc@chaosiq.io>

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -3,13 +3,13 @@
     <div class="container">
       <g-link class="header-logo" to="/">
         <ReliablyLogo />
-        <span class="screen-reader-text">{{ $static.metadata.siteName }}</span>
+        <span class="screen-reader-text">Reliably</span>
       </g-link>
+      <g-link class="header-docs-home" to="/docs">docs</g-link>
       <nav class="nav">
-        <g-link class="nav__link" to="/product">Product</g-link>
-        <g-link class="nav__link" to="/docs/">Docs</g-link>
-        <g-link class="nav__link" to="/resources/">Resources</g-link>
-        <g-link class="nav__link" to="/company/">Company</g-link>
+        <g-link class="nav__link" to="/docs/getting-started/">Getting Started</g-link>
+        <g-link class="nav__link" to="/docs/guides/">Guides</g-link>
+        <g-link class="nav__link" to="/docs/reference">Reference</g-link>
       </nav>
     </div>
   </header>
@@ -51,9 +51,61 @@ export default {
     align-items: center;
   }
 
-  .header-logo {
+  &-logo {
     svg {
       width: 10em;
+    }
+  }
+
+  &-docs-home {
+    display: inline-block;
+    margin-left: .4em;
+    padding: .4em .3em;
+
+    color: var(--brown);
+    font-family: 'Agrandir', sans-serif;
+    font-weight: 700;
+    text-decoration: none;
+    text-transform: uppercase;
+
+    &::before {
+      content: '/';
+
+      display: inline-block;
+      margin-right: .4em;
+
+      transform: translateY(-.1em);
+    }
+  }
+
+  nav {
+    margin-left: 1em;
+
+    a {
+      position: relative;
+
+      margin: 0 1em;
+
+      color: var(--brown);
+      font-weight: 700;
+      text-decoration: none;
+
+      &:hover,
+      &.active {
+        &::after {
+          content: '';
+
+          position: absolute;
+          bottom: -.3rem;
+          left: 0;
+
+          display: block;
+          height: .3rem;
+          width: 100%;
+
+          background-color: var(--red);
+        }
+      }
     }
   }
 }
