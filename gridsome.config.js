61e52c454b18a0785dd7918bfe5ae29694f930c7
Marc Perrien <marc@chaosiq.io>
2021-01-27 13:34:36 +0100

    Add siteUrl.
    Signed-off-by: Marc Perrien <marc@chaosiq.io>

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Reliably',
+  siteName: 'Reliably Documentation',
+  siteUrl: 'http://perrien.fr',
   pathPrefix: '/docs',
 
   chainWebpack(config, { isServer }) {
