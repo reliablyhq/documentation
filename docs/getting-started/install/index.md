fbfb8282c0e147064f0e471bd14b9c961a5d5fbd
Marc Perrien <marc@chaosiq.io>
2021-05-14 09:40:55 +0200

    Finish copy-to-clipboard button.
    Signed-off-by: Marc Perrien <marc@chaosiq.io>

--- docs/getting-started/install/index.md
+++ docs/getting-started/install/index.md
@@ -17,12 +17,10 @@ your PATH.
 
 On macOS
 
-<CopyToClipboard />
-
 ```console
 curl -L https://github.com/reliablyhq/cli/releases/latest/download/reliably-darwin-amd64 -o /usr/local/bin/reliably
 ```
-
+<CopyToClipboard />
 
 On Linux
 
--- src/components/MarkdownCopyToClipboard.vue
+++ src/components/MarkdownCopyToClipboard.vue
@@ -1,10 +1,11 @@
 <template>
   <button
-    class="ctc-button"
+    class="ctc-button button button--small"
     :class="{ copied: displayCopyConfirmation }"
     @click="copyToClipboard"
   >
-    <span class="screen-reader-text">Copy to clipboard</span>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path d="M28.25 20.75h7.5A11.25 11.25 0 0047 9.5a7.5 7.5 0 017.5 7.5v37.5A7.5 7.5 0 0147 62H17a7.5 7.5 0 01-7.5-7.5V17A7.5 7.5 0 0117 9.5a11.25 11.25 0 0011.25 11.25zM24.5 9.5a7.5 7.5 0 0115 0 3.75 3.75 0 01-3.75 3.75h-7.5A3.75 3.75 0 0124.5 9.5z" fill-rule="evenodd"/></svg>
+    {{ label }} <span class="screen-reader-text">to clipboard</span>
   </button>
 </template>
 
@@ -12,32 +13,34 @@
 // First import this component at the begining of your markdown file.
 // import CopyToClipboard from '~/components/MarkdownCopyToClipboard.vue'
 //
-// Then, insert this component _before_ the code block you want it to copy.
+// Then, insert this component _after_ the code block you want it to copy.
 // You MUST let an empty line between the component and the code block.
 // Like this:
-// <CopyToClipboard />
-//
 // ```console
 // reliably slo init
 // ```
+// <CopyToClipboard />
 //
 export default {
   data() {
     return {
+      label: "Copy",
       displayCopyConfirmation: false,
     }
   },
   methods: {
     async copyToClipboard(event) {
-      const target = event.currentTarget.nextSibling;
+      const target = event.currentTarget.previousSibling;
       if (target != null) {
         this.selectText(target);
         document.execCommand('copy');
         target.blur();
         this.displayCopyConfirmation = true;
+        this.label = "Copied!";
         await new Promise( (resolve) => {
           setTimeout(() => {
             this.displayCopyConfirmation = false;
+            this.label = "Copy";
           }, 2000);
         });
       }
@@ -65,19 +68,39 @@ export default {
 
 <style lang="scss" scoped>
 .ctc-button {
-  position: relative;
-  margin-left: 100%;
-  transform: translate(calc(-100% - 1em), calc(100% + 2em));
+  position: absolute;
+  margin-left: 50%;
+  transform: translate(-50%, -1em);
 
-  display: block;
-  height: 2em;
-  width: 2em;
+  // display: block;
+  // height: 2em;
+  // width: 2em;
 
-  background-color: transparent;
-  border: 2px solid red;
+  background-color: var(--yellow);
+  // border: 2px solid red;
+  opacity: 0;
+  cursor: pointer;
+
+  transition: all .2s ease-in-out;
+
+  svg {
+    height: .8em;
+    margin-right: .5em;
+
+    fill: var(--body-color);
+  }
 
   &.copied {
-    border-color: green;
+    background-color: var(--blue-300);
   }
+}
+</style>
+
+<style lang="scss">
+pre:hover + .ctc-button,
+.ctc-button:hover {
+  opacity: 1;
+
+  transform: translate(-50%, calc(-1em - 75%));
 }
 </style>
