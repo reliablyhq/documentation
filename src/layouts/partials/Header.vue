89e3b5442079ac06fe533c834e31548f9c28d94a
Marc Perrien <marc@chaosiq.io>
2021-06-04 10:46:59 +0200

    Import styles from website. Update header
    Signed-off-by: Marc Perrien <marc@chaosiq.io>

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -11,6 +11,12 @@
         <g-link class="nav__link" to="/guides/">Guides</g-link>
         <g-link class="nav__link" to="/reference/">Reference</g-link>
       </nav>
+      <div class="header__github">
+        <a href="https://github.com/reliablyhq/cli" class="button button--icon button--small">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+          Star us on GitHub!
+        </a>
+      </div>
     </div>
   </header>
 </template>
@@ -42,24 +48,29 @@ export default {
   display: flex;
   align-items: center;
   height: auto;
-  padding: .5em 0;
+  padding: .2em 0;
 
-  background-color: var(--bg-transparent);
-  box-shadow: 0 .1rem .6rem rgba(0, 0, 0, .16);
+  background-color: var(--blue-800);
 
-  @media screen and (min-width: 39.5rem) {
-    padding-bottom: 0;
-  }
+  // @media screen and (min-width: 39.5rem) {
+  //   padding-bottom: 0;
+  // }
 
   .container {
     display: flex;
     flex-wrap: wrap;
     align-items: center;
+    max-width: 124rem;
+    padding-right: 2rem; // Allows us to center the menu
+    padding-left: 2rem;
   }
 
   &-logo {
+    display: flex;
+    align-items: center;
+
     svg {
-      width: 10em;
+      height: 2em;
     }
   }
 
@@ -67,11 +78,10 @@ export default {
     position: relative;
 
     display: inline-block;
-    margin-right: 2em;
     margin-left: .4em;
     padding: .4em .3em;
 
-    color: var(--brown);
+    color: var(--heading-color-dark);
     font-family: 'Agrandir', sans-serif;
     font-weight: 700;
     text-decoration: none;
@@ -92,42 +102,42 @@ export default {
       content: '';
 
       position: absolute;
-      top: calc(100% - .1em);
-      left: 1.2em;
+      top: calc(100% - .5em);
+      left: 1.4em;
 
       display: block;
-      height: .2rem;
-      width: calc(100% - 1.4em);
+      height: .3rem;
+      width: calc(100% - 2em);
 
       background-color: transparent;
     }
 
     &:hover {
       &::after {
-        background-color: var(--red);
+        background-color: var(--yellow-500);
       }
     }
   }
 
   nav {
+    margin-right: auto;
+    margin-left: auto;
     @media screen and (min-width: 40rem) {
-      margin-left: 0;
+      // margin-left: 0;
     }
     a {
       position: relative;
 
-      margin: 0 .5em;
+      display: inline-block;
+      padding: 1.2rem;
 
-      color: var(--brown);
-      font-weight: 600;
+      color: white;
+      font-weight: 700;
       text-decoration: none;
 
-      &:first-child {
-        margin-left: 0;
-      }
-
-      &:last-child {
-        margin-right: 0;
+      @media (min-width: 30rem) {
+        margin: 0 .5em;
+        padding: 1em;
       }
 
       &:hover,
@@ -136,14 +146,14 @@ export default {
           content: '';
 
           position: absolute;
-          bottom: -1.1rem;
-          left: 0;
+          top: calc(100% - .5em);
+          left: 1em;
 
           display: block;
-          height: .2rem;
-          width: 100%;
+          height: .3rem;
+          width: calc(100% - 2em);
 
-          background-color: var(--red);
+          background-color: var(--yellow-500);
         }
       }
     }
